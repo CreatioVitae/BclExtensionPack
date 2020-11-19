e77f5f1931c3c98934ef07acd265f955a2041ca7
CreatioVitae <h.chiba201103@gmail.com>
2020-11-19 21:39:54 +0900

    Update NameSpace...

--- src/BclExtensionPack.CoreLib/EnumerableExtensions.cs
+++ src/BclExtensionPack.CoreLib/EnumerableExtensions.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
-using System.Linq;
 
-namespace BclExtensionPack.CoreLib {
+namespace System.Linq {
     public static class EnumerableExtensions {
         public static List<T> AsList<T>(this IEnumerable<T> source) where T : class =>
             (source is List<T> list) ? list : source.ToList();
