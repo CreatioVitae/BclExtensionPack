91d3ffdd1eecbe2ca4db63eefa66205756890482
CreatioVitae <h.chiba201103@gmail.com>
2021-03-15 23:24:03 +0900

    add LoopAdd...

--- src/BclExtensionPack.CoreLib/EnumerableExtensions.cs
+++ src/BclExtensionPack.CoreLib/EnumerableExtensions.cs
@@ -37,5 +37,11 @@ namespace System.Linq {
 
             await Task.WhenAll(tasks.ToArray()).ConfigureAwait(configureAwait);
         }
+
+        public static void LoopAdd<T>(this List<T> collection, T item, int loopCount) {
+            for (var i = 1; i <= loopCount; i++) {
+                collection.Add(item);
+            }
+        }
     }
 }
