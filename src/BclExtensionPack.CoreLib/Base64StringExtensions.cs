f0d524cc4adbb526c4eae9fef0e5eeb478c715c3
CreatioVitae <h.chiba201103@gmail.com>
2021-08-16 22:39:46 +0900

    clean : add Nullability Attribute...

---
+++ src/BclExtensionPack.CoreLib/Base64StringExtensions.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace System {
+    public static class Base64StringExtensions {
+        public static bool TryDecodeUtf8FromBase64String(this string base64, [NotNullWhen(true)] out string? utf8) {
+            Span<byte> bytes = stackalloc byte[256];
+            bool result;
+
+            (result, utf8) = Convert.TryFromBase64String(base64, bytes, out var bytesWritten)
+                ? (true, Encoding.UTF8.GetString(bytes.Slice(0, bytesWritten)))
+                : (false, null);
+
+            return result;
+        }
+    }
+}
--- src/BclExtensionPack.CoreLib/StringExtensions.cs
+++ src/BclExtensionPack.CoreLib/StringExtensions.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Text;
 
 namespace System {
     public static class StringExtensions {
@@ -8,16 +7,5 @@ namespace System {
 
         public static bool IsNotNullAndNotWhiteSpace([NotNullWhen(true)] this string? s) =>
             !string.IsNullOrWhiteSpace(s);
-
-        public static bool TryDecodeUtf8FromBase64String(this string base64, out string? utf8) {
-            Span<byte> bytes = stackalloc byte[256];
-            bool result;
-
-            (result, utf8) = Convert.TryFromBase64String(base64, bytes, out var bytesWritten)
-                ? (true, Encoding.UTF8.GetString(bytes.Slice(0, bytesWritten)))
-                : (false, null);
-
-            return result;
-        }
     }
 }
