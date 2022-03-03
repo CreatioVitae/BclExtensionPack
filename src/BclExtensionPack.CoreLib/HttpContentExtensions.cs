a6375f2d62c22dc117c9e899e62b399734bf2188
CreatioVitae <h.chiba201103@gmail.com>
2022-03-03 16:13:39 +0900

    repair Get<T> method...

--- src/BclExtensionPack.CoreLib/HttpContentExtensions.cs
+++ src/BclExtensionPack.CoreLib/HttpContentExtensions.cs
@@ -3,7 +3,7 @@ using Utf8Json.Resolvers;
 
 namespace System.Net.Http; 
 public static class HttpContentExtensions {
-    public static async ValueTask<T> Get<T>(HttpContent content, IJsonFormatterResolver? resolver = null) {
+    public static async ValueTask<T> Get<T>(this HttpContent content, IJsonFormatterResolver? resolver = null) {
         resolver ??= StandardResolver.Default;
 
         return JsonSerializer.Deserialize<T>(await content.ReadAsByteArrayAsync(), resolver);
