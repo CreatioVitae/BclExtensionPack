9bdf6b9c96ce2e4015d4c06363af5457dfd5f38d
hchiba1984 <h.chiba201103@gmail.com>
2022-03-11 18:53:45 +0900

    add HttpContent Extension... (#6)

---
+++ src/BclExtensionPack.CoreLib/HttpContentExtensions.cs
@@ -0,0 +1,11 @@
+using Utf8Json;
+using Utf8Json.Resolvers;
+
+namespace System.Net.Http; 
+public static class HttpContentExtensions {
+    public static async ValueTask<T> Get<T>(this HttpContent content, IJsonFormatterResolver? resolver = null) {
+        resolver ??= StandardResolver.Default;
+
+        return JsonSerializer.Deserialize<T>(await content.ReadAsByteArrayAsync(), resolver);
+    }
+}
