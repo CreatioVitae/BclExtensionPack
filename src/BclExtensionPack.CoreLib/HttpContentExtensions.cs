df39df52883654e6759aa2e5b769a07bb4a2aa1f
CreatioVitae <h.chiba201103@gmail.com>
2022-03-11 20:22:20 +0900

    add CreateJsonHttpContent<T>

--- src/BclExtensionPack.CoreLib/HttpContentExtensions.cs
+++ src/BclExtensionPack.CoreLib/HttpContentExtensions.cs
@@ -1,11 +1,24 @@
+using System.Net.Http.Headers;
 using Utf8Json;
 using Utf8Json.Resolvers;
 
-namespace System.Net.Http; 
+namespace System.Net.Http;
 public static class HttpContentExtensions {
-    public static async ValueTask<T> Get<T>(this HttpContent content, IJsonFormatterResolver? resolver = null) {
+    public static async ValueTask<T> GetAsync<T>(this HttpContent content, IJsonFormatterResolver? resolver = null) {
         resolver ??= StandardResolver.Default;
 
         return JsonSerializer.Deserialize<T>(await content.ReadAsByteArrayAsync(), resolver);
+    }
+
+    public static HttpContent CreateJsonHttpContent<T>(this T value, IJsonFormatterResolver? resolver = null) where T : class {
+        resolver ??= StandardResolver.Default;
+
+        var byteArray = JsonSerializer.Serialize(value, resolver);
+
+        var content = new ByteArrayContent(byteArray);
+
+        content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+        return content;
     }
 }
