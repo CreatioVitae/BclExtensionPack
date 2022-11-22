4cfc531a0761140f5f562be8899adc0b3e004841
CreatioVitae <h.chiba201103@gmail.com>
2022-11-22 22:32:40 +0900

    Move ConfigurationExtensions from WebPack.CoreLib.

---
+++ src/BclExtensionPack.CoreLib/ConfigurationExtensions.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration;
+using System.ComponentModel.DataAnnotations;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+public static class ConfigurationExtensions {
+    public static T GetAvailable<T>(this IConfigurationSection configuration) {
+        var obj = configuration.Get<T>();
+
+        ArgumentNullException.ThrowIfNull(obj);
+
+        Validator.ValidateObject(obj, new ValidationContext(obj), true);
+        return obj;
+    }
+
+    public static string GetAvailableValueFromKey(this IConfiguration configuration, string key) {
+        var valueOrDefault = configuration[key];
+
+        ArgumentNullException.ThrowIfNull(valueOrDefault);
+
+        return valueOrDefault;
+    }
+}
